2af861fa601ff8dc3da8e59526ebfdaf00d1574b
Aaron Dsouza <aaron.dsouza@honeywell.com>
2019-08-03 15:04:58 +0530

    Final changes for the demo

---
+++ Ironman3/MasterViewControllerTableViewController.h
@@ -0,0 +1,17 @@
+//
+//  MasterViewControllerTableViewController.h
+//  Ironman3
+//
+//  Created by Aaron D'Souza on 14/06/19.
+//  Copyright © 2019 Aaron D'Souza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MasterViewControllerTableViewController : UITableViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
